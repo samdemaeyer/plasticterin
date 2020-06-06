59d00addd18597b1502639424440e251ff78207d
Sam De Maeyer <sam_demaeyer@hotmail.com>
2020-06-06 18:13:25 +0100

    Updates

---
+++ precache-manifest.44dbc1ba2db7f9a5b9ca89e6bc4997b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525197fc0458aff060be38727c842381",
+    "url": "/plasticterin/index.html"
+  },
+  {
+    "revision": "4ea98769484c403b7ff1",
+    "url": "/plasticterin/static/css/main.a0636428.chunk.css"
+  },
+  {
+    "revision": "e2d1fb937095335dd8aa",
+    "url": "/plasticterin/static/js/2.72ac9b38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/plasticterin/static/js/2.72ac9b38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ea98769484c403b7ff1",
+    "url": "/plasticterin/static/js/main.9e4e1147.chunk.js"
+  },
+  {
+    "revision": "eac6bc7d008e0bd872e0",
+    "url": "/plasticterin/static/js/runtime-main.66cb3aa9.js"
+  }
+]);
