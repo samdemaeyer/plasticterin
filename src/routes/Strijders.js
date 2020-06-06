b53147542d48636b68bef06afd38d8c97d3c06d5
Sam De Maeyer <sam_demaeyer@hotmail.com>
2020-06-06 21:49:47 +0100

    Add images

--- src/routes/Strijders.js
+++ src/routes/Strijders.js
@@ -10,26 +10,26 @@ export default function Strijders() {
         <article className="article">
           <div className="content">
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Workshops" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-1.png" alt="Workshops" />
               Workshops
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Eccosentials" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-4.png" alt="Eccosentials" />
               Eccosentials
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Clean Ups" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-7.png" alt="Clean Ups" />
               Clean Ups
             </Link>
           </div>
 
           <div className="content">
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Gezakt" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-2.png" alt="Gezakt" />
               Gepakt en Gezakt
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Antwerp" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-5.png" alt="Antwerp" />
               To Be Antwerp
             </Link>
             <Link to="/" className="project-wrapper">
@@ -40,11 +40,11 @@ export default function Strijders() {
 
           <div className="content">
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Trashwerp" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-3.png" alt="Trashwerp" />
               Trashwerp
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="vrij" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-6.png" alt="vrij" />
               Mei plastic vrij
             </Link>
             <Link to="/" className="project-wrapper">
@@ -60,153 +60,153 @@ export default function Strijders() {
         <article className="article">
           <div className="content">
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Ineos will fall" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-8.png" alt="Ineos will fall" />
               Ineos will fall
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Mei plastic vrij" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-11.png" alt="Mei plastic vrij" />
               Mei plastic vrij
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Flagbag" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-14.png" alt="Flagbag" />
               Flagbag
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Plastic soup foundation" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-17.png" alt="Plastic soup foundation" />
               Plastic soup foundation
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="eneco clean beach" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-20.png" alt="eneco clean beach" />
               eneco clean beach
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="W.r. yuma" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-23.png" alt="W.r. yuma" />
               W.r. yuma
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Freitag" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-26.png" alt="Freitag" />
               Freitag
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Zero plastic rivers" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-29.png" alt="Zero plastic rivers" />
               Zero plastic rivers
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="fair and square" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-32.png" alt="fair and square" />
               fair and square
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Dopper" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-35.png" alt="Dopper" />
               Dopper
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="precious plastic" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-38.png" alt="precious plastic" />
               precious plastic
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Low impact movement" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-41.png" alt="Low impact movement" />
               Low impact movement
             </Link>
           </div>
 
           <div className="content">
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Washed up" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-9.png" alt="Washed up" />
               Washed up
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="oplossing" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-12.png" alt="oplossing" />
               Stormkop
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="oplossing" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-15.png" alt="oplossing" />
               Plastic attack
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="oplossing" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-18.png" alt="oplossing" />
               The ocean clean up
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="oplossing" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-21.png" alt="oplossing" />
               Ecossentials
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="oplossing" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-24.png" alt="oplossing" />
               Minimal
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="oplossing" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-27.png" alt="oplossing" />
               Got bag
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="oplossing" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-30.png" alt="oplossing" />
               Plastic Maritimus
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="oplossing" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-33.png" alt="oplossing" />
               Plastic Reef
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="oplossing" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-36.png" alt="oplossing" />
               wasted reality
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="oplossing" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-39.png" alt="oplossing" />
               Zero waste club
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="oplossing" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-42.png" alt="oplossing" />
               Weefwijven
             </Link>
           </div>
 
           <div className="content">
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="two minutes of earth" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-10.png" alt="two minutes of earth" />
               two minutes of earth
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Extinction rebellion" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-13.png" alt="Extinction rebellion" />
               Extinction rebellion
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Plastic Avengers" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-16.png" alt="Plastic Avengers" />
               Plastic Avengers
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Plastic fisher" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-19.png" alt="Plastic fisher" />
               Plastic fisher
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Laura from the dessert" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-22.png" alt="Laura from the dessert" />
               Laura from the dessert
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Fairphone" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-25.png" alt="Fairphone" />
               Fairphone
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="River Cleanup" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-28.png" alt="River Cleanup" />
               River Cleanup
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Trashworksdesign" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-31.png" alt="Trashworksdesign" />
               Trashworksdesign
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Beo bottle" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-34.png" alt="Beo bottle" />
               Beo bottle
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Running the numbers" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-37.png" alt="Running the numbers" />
               Running the numbers
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Maria Arceo" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-40.png" alt="Maria Arceo" />
               Maria Arceo
             </Link>
             <Link to="/" className="project-wrapper">
-              <img className="project-thumb missing" src="/plasticterin/projects/MISSING" alt="Dianna Cohen" />
+              <img className="project-thumb" src="/plasticterin/projects/strijders-43.png" alt="Dianna Cohen" />
               Dianna Cohen
             </Link>
           </div>
