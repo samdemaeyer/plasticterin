1616ad5da0ca738049bbb92b21363dca9ca98e22
Ben De Maeyer <demaeyerben@gmail.com>
2020-06-09 13:03:43 +0200

    inspringen vijfnatwaalf alineas

--- src/routes/VijfNaTwaalf.js
+++ src/routes/VijfNaTwaalf.js
@@ -14,18 +14,21 @@ export default function VijfNaTwaalf() {
         gevolgen voor planeet aarde.
       </p>
       <p>
+      <span className="block-space"></span>
         Hierbij nodigen wij iedereen uit om samen te wachten. Samen te wachten op een oplossing.
         Want dat is toch wat we eigenlijk allemaal doen? Uiteraard zijn er tal van geweldige initiatieven zoals
         Mei plastic vrij en waren er reeds massaal protesten van zowel jong als oud. En toch.
         Toch veranderde er niet voldoende.
       </p>
       <p>
+      <span className="block-space"></span>
         Wanneer mei voorbij is, minderen we wel met plastic, maar helemaal stoppen is moeilijk.
         Dat geven we toe, wij kunnen het ook niet helemaal. Ook wij falen daarin. Maar er is meer voor
         nodig om ons niet genoeg te kunnen voorzien met alternatieven. Alle jongeren die op straat kwamen, voor wat?
         Voor niets? Het mag niet voor niets zijn geweest!
       </p>
       <p>
+      <span className="block-space"></span>
         Daarom is het tijd voor verandering. Gedaan met het wachten, gedaan met het wachten op een oplossing.
         Het is tijd. Nu. Want het is al 5 na 12. Wij willen verandering en wel NU. Voordat het te laat is.
       </p>
