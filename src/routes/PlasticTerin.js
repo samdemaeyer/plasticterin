b01cbbe869821694f201164d6cd11f6d88002a44
Ben De Maeyer <demaeyerben@gmail.com>
2020-06-10 17:30:19 +0200

    changes text

--- src/routes/PlasticTerin.js
+++ src/routes/PlasticTerin.js
@@ -9,54 +9,46 @@ export default function VijfNaTwaalf() {
         <div className="content">
           <p>
             <span className="block-space"></span>
-            Het is 5 na 12, wat zeggen we? Zelfs kwart na 12. WE NEED ACTION. Gedaan met naar elkaar wijzen,
-            gedaan met wachten. Het moet nu gebeuren en geen excuses meer. Greta en Anuna hebben niet voor niets al
-            die weken gespijbeld, niet voor niets betoogd en vooral: hebben niet voor niets geprobeerd om de politiek
-            aan te spreken. Wetten rond het gebruik van plastic zijn niet voldoende, ook de plastic productie moet
-            worden aangepakt. De klimaatproblematiek is tegenwoordig een ‘hot topic’ en na het succesvol afsluiten
-            van de periode  ‘mei plasticvrij 2020’  mogen we de inspanningen voor de plasticvrije periode nog niet
-            opgeven maar doorzetten. Het zou gek zijn om nu op te geven toch?
+            Het is 5 na 12, wat zeggen we? Zelfs kwart na 12. WE NEED ACTION. Gedaan met naar elkaar wijzen, gedaan met wachten. 
+            Het moet nu gebeuren en geen excuses meer. Greta en Anuna hebben niet voor niets al die weken gespijbeld, 
+            niet voor niets betoogd en vooral: hebben niet voor niets geprobeerd om de politiek aan te spreken. 
+            Wetten rond het gebruik van plastic zijn niet voldoende, ook de plastic productie moet worden aangepakt. 
+            De klimaatproblematiek is tegenwoordig een ‘hot topic’ en na het succesvol afsluiten van de periode ‘mei plasticvrij 2020’, 
+            mogen we de inspanningen voor de plasticvrije periode nog niet opgeven, we moeten net doorzetten. Het zou gek zijn om nu op te geven, toch?
           </p>
           <h4 className="title">Wie?</h4>
           <p>
             <span className="block-space"></span>
-            Wij zijn Carla-Reina en Dorina van ‘Plastic(erin)’, twee studenten van Sint Lucas Antwerpen.
-            We hebben onze buik  vol van plastic en voeren daarom een sensibiliseringscampagne rond het
-            plastic verbruik. Onze interesses zijn gelijklopend en door meer te lezen over
-            het onderwerpkregen we steeds meer  het gevoel dat er iets moest gebeuren, iets dringends.
-            Het kan zo niet langer. Iedereen weet dat er iets moet veranderen, maar het moet van alle kanten komen.
-            We mogen niet langer met de vinger naar elkaar wijzen, maar proberen om samen naar een oplossing te
-            zoeken en daar hebben we jou ook voor nodig!
+            Wij zijn Carla-Reina en Dorina van ‘Plastic(terin)’, twee studenten van Sint Lucas Antwerpen. We hebben onze buik vol van plastic en voeren daarom een 
+            sensibiliseringscampagne rond het plastic verbruik. Onze interesses zijn gelijklopend en door meer te lezen over dit onderwerp, 
+            kregen we steeds meer het gevoel dat er iets moest gebeuren en wel dringend. Het kan zo niet langer. Iedereen weet dat er iets moet veranderen, 
+            maar het moet van alle kanten komen. We mogen niet langer met de vinger naar elkaar wijzen, maar we moeten proberen om samen naar een oplossing te zoeken 
+            en daar hebben we jou ook voor nodig!
           </p>
           <h4 className="title">Betekenis?</h4>
           <p>
             <span className="block-space"></span>
-            Plastic(terin) komt van “Stikt er in”. Hiermee bedoelen we dat de plasticproblematiek enorm groot
-            is en we het niet mogen negeren. Daarom kozen we ervoor om een samentrekking te nemen van het woord
-            ‘plastic’ en ‘erin stikken’. Zo kwamen we bij Pla-stict-er-in.
+            Plastic(terin) komt van “Stikt er in”. Hiermee bedoelen we dat de plastic problematiek enorm groot is en we het niet mogen negeren. 
+            Daarom kozen we ervoor om een samengesteld woord te maken van het woord ‘plastic’ en ‘erin stikken’. Zo kwamen we bij Pla-stict-er-in.
           </p>
         </div>
         <div className="content">
           <h4 className="title">Wat?</h4>
           <p>
             <span className="block-space"></span>
-            Via deze campagne proberen we consumenten te  sensibiliseren overde huidige klimaatproblematiek en
-            duidelijk te maken dat het 5 na 12 is en dus hoog tijd voor verandering. Onze oceanen zitten vol,
-            vol met plastic. Op dit platform vind je onder andere tips en weetjes, verschillende projecten die we het
-            afgelopen jaar op poten hebben gezet én ook onze zoektocht naar alternatieven. Zo proberen we jou ook te
-            overtuigen met laagdrempelige initiatieven, zodat ook jij een steentje kan bijdragen en zo deel kan
-            uitmaken van de oplossing. Je zal zien, het is echt niet moeilijk en het werkt verslavend, je wil vanzelf
-            meer en beter je best doen voor onze aarde.
+            Via deze campagne proberen we consumenten te sensibiliseren over de huidige klimaatproblematiek en duidelijk te maken dat het 5 na 12 is en dus 
+            hoog tijd voor verandering. Onze oceanen zitten vol, vol met plastic. Op dit platform vind je onder andere tips en weetjes, verschillende projecten 
+            die we het afgelopen jaar op poten hebben gezet én ook onze zoektocht naar alternatieven. Zo proberen we jou ook te overtuigen met 
+            laagdrempelige initiatieven, zodat ook jij een steentje kan bijdragen en zo deel kan uitmaken van de oplossing. Je zal zien, 
+            het is echt niet moeilijk en het werkt verslavend! Je wil vanzelf meer en beter je best doen voor onze aarde.
           </p>
           <p>
             <span className="block-space"></span>
-            Wij doen dit vooral omdat we merken dat de stap naar plasticvrij leven groot is en mensen vaak niet
-            weten waar te beginnen. Omdat we dikwijls overspoeld worden met cijfers over de hoeveelheid afval er
-            geproduceerd wordt, zien we door het bos de bomen niet meer, maar geen paniek. We gaan telkens aan de
-            slag met laagdrempelige stappen aangezien wij van mening zijn dat het geen wedstrijd is en dat alle
-            beetjes helpen. Ook wij zijn niet perfect, maar we proberen vooral ons best te doen. En vooral omdat
-            de handelingen gemakkelijk en aangenaam moeten blijven en niet tegenwerken. Er is nood aan verduidelijking
-            en met dit platform willen we jullie hier graag in begeleiden.
+            Wij doen dit vooral omdat we merken dat de stap naar plasticvrij leven groot is en mensen vaak niet weten waar te beginnen. 
+            Omdat we dikwijls overspoeld worden met cijfers over de hoeveelheid afval er geproduceerd wordt, zien we door het bos de bomen niet meer. 
+            Maar geen paniek! We gaan telkens aan de slag met laagdrempelige stappen aangezien wij van mening zijn dat het geen wedstrijd is en dat alle beetjes helpen. 
+            Ook wij zijn niet perfect, maar we proberen vooral ons best te doen. We vinden het ook belangrijk dat de te volgen handelingen steeds aangenaam blijven. 
+            Er is nood aan verduidelijking en met dit platform willen we jullie hier graag in begeleiden.
           </p>
         </div>
       </article>
